ce967b676ac6a08bd4e9b97fbbf28fafd9932db3
Zouhri53 <zouhri53@gmail.com>
2023-02-02 22:34:51 +0100

    modified:   src/app/user-detail/user-detail.component.ts

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -15,18 +15,7 @@ export class UserDetailComponent implements OnInit {
   usId: string = ''
   userName: string = ''
   userLastName: string = ''
-  
-  userAccountUpdate = new FormGroup({
-      nom: new FormControl('', [Validators.required,Validators.maxLength(12)]),
-      prenom: new FormControl('', [Validators.required]),
-      rue: new FormControl('', [Validators.required]),
-      cp: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]),
-      ville: new FormControl('', [Validators.required]),
-      fonction: new FormControl('', [Validators.required]),
-      phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]),
-      email: new FormControl('', [Validators.required,Validators.email])
-  })
-
+  userAccountUpdate: FormGroup | any
 
   constructor( private userService:userServicesService, private route:Router, private activeroute:ActivatedRoute ) 
     { }
@@ -35,7 +24,6 @@ export class UserDetailComponent implements OnInit {
 
     this.activeroute.paramMap.subscribe((params) => { 
       this.usId = params.get('userId')!
-
     })
 
     this.getUserByPostedId(Number(this.usId))
@@ -51,19 +39,19 @@ export class UserDetailComponent implements OnInit {
   getUserByPostedId(postedId:number)
   {
       let getUser = this.userService.getUserById(postedId)
-
       this.userName = getUser!.prenom
       this.userLastName = getUser!.nom
 
-      this.userAccountUpdate.controls['nom'].setValue(getUser!.nom)
-      this.userAccountUpdate.controls['prenom'].setValue(getUser!.prenom)
-      this.userAccountUpdate.controls['rue'].setValue(getUser!.adresse)
-      this.userAccountUpdate.controls['cp'].setValue(getUser!.cp)
-      this.userAccountUpdate.controls['fonction'].setValue(getUser!.fonction)
-      this.userAccountUpdate.controls['ville'].setValue(getUser!.ville)
-      this.userAccountUpdate.controls['phone'].setValue(getUser!.phone)
-      this.userAccountUpdate.controls['email'].setValue(getUser!.email)
-
+      this.userAccountUpdate = new FormGroup({
+        nom: new FormControl(getUser!.nom, [Validators.required,Validators.maxLength(12)]),
+        prenom: new FormControl(getUser!.prenom, [Validators.required]),
+        rue: new FormControl(getUser?.adresse, [Validators.required]),
+        cp: new FormControl(getUser!.cp, [Validators.required, Validators.pattern("^[0-9]*$")]),
+        ville: new FormControl(getUser!.ville, [Validators.required]),
+        fonction: new FormControl(getUser!.fonction, [Validators.required]),
+        phone: new FormControl(getUser!.phone, [Validators.required, Validators.pattern("^[0-9]*$")]),
+        email: new FormControl(getUser!.email, [Validators.required,Validators.email])
+    })
   }
 
   deleteUser()
